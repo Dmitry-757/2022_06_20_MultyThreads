129261e6baa3a7dacb7d59fdd0cf3f1ca90fdddc
Dmitry-757 <glv11.glv11.glv11@gmail.com>
2022-06-22 17:28:02 +0300

    try/finally

--- src/org/dng/Locks.java
+++ src/org/dng/Locks.java
@@ -1,5 +1,6 @@
 package org.dng;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -10,102 +11,117 @@ public class Locks {
         //final Lock lock = new ReentrantLock();
 
 
-        new Thread(new Runnable(){
+        new Thread(new Runnable() {
             @Override
             public void run() {
-                while (true){
-                    //System.out.println(Thread.currentThread().getName()+" try to take");
-                    if (fork1.take()) {
+                while (true) {
+                    System.out.println(Thread.currentThread().getName()+" try to take fork");
+                    if (fork1.lock.tryLock()) {
                         try {
-                            Thread.sleep(10000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
+                            System.out.println(Thread.currentThread().getName() + " took the fork");
+                            System.out.println(Thread.currentThread().getName() + " eating....");
+                            try {
+                                Thread.sleep(10000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            System.out.println(Thread.currentThread().getName() + " put the fork");
+                        } finally {
+                            fork1.lock.unlock();
                         }
-                        //System.out.println(Thread.currentThread().getName()+"try put");
-                        fork1.put();
-
-                        System.out.println(Thread.currentThread().getName()+" thinking....");
-                        try {
-                            Thread.sleep(12000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+                    } else {
+                        System.out.println(Thread.currentThread().getName() + " Fork is busy, waiting...");
+//                        try {
+//                            Thread.sleep(4000);
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
 
                     }
-                    else
-                    {
-                        System.out.println(Thread.currentThread().getName()+" Fork is busy, waiting...");
-                        try {
-                            Thread.sleep(7000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+
+                    System.out.println(Thread.currentThread().getName() + " thinking....");
+                    try {
+                        Thread.sleep(4000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
+
                 }
             }
-        },"Ph1").start();
+        }, "Ph1").start();
 
-        new Thread(new Runnable(){
+        new Thread(new Runnable() {
             @Override
             public void run() {
-                while (true){
-                    //System.out.println(Thread.currentThread().getName()+" try to take");
-                    if (fork1.take()) {
+                while (true) {
+                    System.out.println(Thread.currentThread().getName()+" try to take fork");
+                    if (fork1.lock.tryLock()) {
                         try {
-                            Thread.sleep(9000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
+                            System.out.println(Thread.currentThread().getName() + " took the fork");
+                            System.out.println(Thread.currentThread().getName() + " eating....");
+                            try {
+                                Thread.sleep(10000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            System.out.println(Thread.currentThread().getName() + " put the fork");
+                        } finally {
+                            fork1.lock.unlock();
                         }
-                        //System.out.println(Thread.currentThread().getName()+"try put");
-                        fork1.put();
-
-                        System.out.println(Thread.currentThread().getName()+" thinking....");
-                        try {
-                            Thread.sleep(10000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+                    } else {
+                        System.out.println(Thread.currentThread().getName() + " Fork is busy, waiting...");
+//                        try {
+//                            Thread.sleep(4000);
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
 
                     }
-                    else
-                    {
-                        System.out.println(Thread.currentThread().getName()+" Fork is busy, waiting...");
-                        try {
-                            Thread.sleep(6000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+
+                    System.out.println(Thread.currentThread().getName() + " thinking....");
+                    try {
+                        Thread.sleep(4000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
+
                 }
             }
-        },"Ph2").start();
+        }, "Ph2").start();
 
     }
 }
 
-class Fork{
-    private boolean busy;
-    private final Lock lock = new ReentrantLock();
+class Fork {
+    private volatile boolean busy;
+    public final Lock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
 
-    public boolean take(){
-        System.out.println(Thread.currentThread().getName()+" try take the fork");
-        if (lock.tryLock()){
+    public boolean take() {
+        System.out.println(Thread.currentThread().getName() + " try take the fork");
+        if (lock.tryLock()) {
             busy = true;
             System.out.println(Thread.currentThread().getName() + " took the fork");
+            System.out.println(Thread.currentThread().getName() + " eating...");
             return true;
-        }
-        else{
+        } else {
             //System.out.println("Fork is busy");
             busy = false;
+//            try {
+//                condition.await();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
             return false;
         }
+//        return busy;
     }
 
-    public boolean put(){
+    public void put() {
         busy = false;
         System.out.println(Thread.currentThread().getName() + " put the fork");
+//        condition.signalAll();
         lock.unlock();
-        return false;
     }
 }
 
